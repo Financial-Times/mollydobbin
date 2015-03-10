837f034fcd719da4927f9798582439cfa17c5396
Adam Braimbridge <adam.braimbridge@ft.com>
2015-03-10 18:45:05 +0000

    Moved all files into src directory; added extention crx and pem

--- background.js
+++
@@ -1,9 +0,0 @@
-var trackableElementTypes = ['a','button','select','input','textarea'];
-var trackableElements = $(trackableElementTypes);
-var trackedElements = $('[data-trackable]');
-
-var untrackedElements;
-trackableElementTypes.forEach(function(el) {
-	$(el+':not([data-trackable])').css('background','red');
-});
-
---
+++ src/background.js
@@ -0,0 +1,22 @@
+//if(!(window.jQuery && window.jQuery.fn.jquery == '1.3.2')) {var s = document.createElement('script');s.setAttribute('src', 'http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js');s.setAttribute('type', 'text/javascript');document.getElementsByTagName('head')[0].appendChild(s);}
+
+var traps = ['a','button','select','input','textarea'];
+//var containers = ['ft-paragraph','p','div','span','table','ul','ol','li'];
+
+// Expose all elements that ought to be tracked but are not
+traps.forEach(function(tag) {
+	$(tag+':not([data-trackable])').css('background','red');
+});
+
+// Outline all of the deepest trackable elements (traps) in one style, and
+// all of the parent trackable elements (containers) in another.
+var trackedElements = [].slice.call(document.querySelectorAll('[data-trackable]'));
+trackedElements.forEach(function(node){
+	if(node.querySelectorAll(traps).length === 0){
+		node.style.border="2px dotted red";
+	} else {
+		node.style.border="1px dotted blue";
+	}
+});
+
+//$(node).parents().length
