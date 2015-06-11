0641a9adb621aff65310cb44317fb57f319023ce
Braimbridge, Adam <Adam.Braimbridge@ft.com>
2015-06-11 15:27:33 +0000

    Update src/content_script.js

--- src/content_script.js
+++ src/content_script.js
@@ -9,7 +9,7 @@
 	// Expose all elements that ought to be tracked but are not
 	var highlightUntrackedElements=function(){
 		traps.forEach(function(tag){
-			$(tag+':not([data-trackable]):not([data-toggle])').css({
+			$(tag+':not([data-trackable]):not([data-toggle])').filter(":visible").css({
 				'box-sizing':'border-box',
 				'border':'2px solid lime'
 			});
